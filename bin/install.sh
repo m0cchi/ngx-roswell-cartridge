1d3d6bbc60f044967a97a7b7a991d1dc107a794f
mocchi <boom.boom.planet@gmail.com>
2015-10-12 12:29:40 +0900

    rename

--- bin/install.sh
+++ bin/install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-BIN=$OPENSHIFT_SBCL_DIR/bin
-export PATH=$OPENSHIFT_SBCL_DIR/bin:$PATH
-export HOME=$OPENSHIFT_SBCL_DIR/home
+BIN=$OPENSHIFT_ROSWELL_DIR/bin
+export PATH=$OPENSHIFT_ROSWELL_DIR/bin:$PATH
+export HOME=$OPENSHIFT_ROSWELL_DIR/home
 export ROSWELL_HOME=$HOME
 
-cd $OPENSHIFT_SBCL_DIR
+cd $OPENSHIFT_ROSWELL_DIR
 mkdir temp
 wget ftp://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 tar zxvf autoconf-2.69.tar.gz
 cd autoconf-2.69
-./configure --prefix=$OPENSHIFT_SBCL_DIR
+./configure --prefix=$OPENSHIFT_ROSWELL_DIR
 make && make install
 
 cd ../
 git clone https://github.com/snmsts/roswell.git
 cd roswell
 sh bootstrap
-./configure --prefix=$OPENSHIFT_SBCL_DIR
+./configure --prefix=$OPENSHIFT_ROSWELL_DIR
 make && make install
 ros setup
 ros install sbcl-bin
